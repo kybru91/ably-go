03f0dc3334877328752be03bd4d8c38cf4ad0d4f
Morgan Adamiec <morgan.adamiec@ably.com>
2022-05-05 16:14:05 +0100

    Refactored code and tests for TM3 message encoding/decoding

--- ably/proto_message_test.go
+++ ably/proto_message_test.go
@@ -9,6 +9,7 @@ import (
 	"testing"
 
 	"github.com/ably/ably-go/ably"
+	"github.com/ably/ably-go/ablytest"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -19,7 +20,7 @@ func (custom) MarshalJSON() ([]byte, error) {
 	return json.Marshal([]string{"custom"})
 }
 
-func TestMessage(t *testing.T) {
+func TestMessage_EncodeDecode_TM3(t *testing.T) {
 	key, err := base64.StdEncoding.DecodeString("WUP6u0K7MXI5Zeo0VppPwg==")
 	assert.NoError(t, err)
 	iv, err := base64.StdEncoding.DecodeString("HO4cYSP8LybPYBPZPHQOtg==")
@@ -121,6 +122,55 @@ func TestMessage(t *testing.T) {
 			assert.NoError(t, err)
 			assert.Equal(t, v.decoded, decoded.Data,
 				"expected %#v got %#v", v.decoded, decoded.Data)
+		})
+	}
+}
+
+func TestMessage_CryptoDataFixtures_RSL6a1_RSL5b_RSL5c_TM3(t *testing.T) {
+	fixtures := []struct {
+		desc, file string
+		keylength  int
+	}{
+		{"with a 128 keylength", "test-resources/crypto-data-128.json", 128},
+		{"with a 256 keylength", "test-resources/crypto-data-256.json", 126},
+	}
+
+	for _, fixture := range fixtures {
+		// pin
+		fixture := fixture
+		t.Run(fixture.desc, func(t *testing.T) {
+			test, key, iv, err := ablytest.LoadCryptoData(fixture.file)
+			assert.NoError(t, err)
+
+			params := ably.CipherParams{
+				Algorithm: ably.CipherAES,
+				Key:       key,
+			}
+			params.SetIV(iv)
+			opts := &ably.ProtoChannelOptions{
+				Cipher: params,
+			}
+			t.Run("fixture encode", func(t *testing.T) {
+				for _, item := range test.Items {
+					cipher, _ := opts.GetCipher()
+
+					var encoded ably.Message
+					err := json.Unmarshal(item.Encoded, &encoded)
+					assert.NoError(t, err)
+
+					encoded, err = ably.MessageWithDecodedData(encoded, cipher)
+					assert.NoError(t, err)
+
+					var encrypted ably.Message
+					err = json.Unmarshal(item.Encoded, &encrypted)
+					assert.NoError(t, err)
+
+					encrypted, err = ably.MessageWithDecodedData(encrypted, cipher)
+					assert.NoError(t, err)
+					assert.Equal(t, encoded.Name, encrypted.Name)
+					assert.Equal(t, encoded.Data, encrypted.Data)
+				}
+			})
 		})
 	}
 }
